61996061005e2391841e6c39cb5929128bbc8ec3
mynameisaist <ghtv1523@mail.ru>
2022-09-29 00:18:39 +0300

    dev

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/admin/Desktop/crc32/googletest/googlemock
-# Build directory: C:/Users/admin/Desktop/crc32/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- tests/crc32tests.cpp
+++ tests/crc32tests.cpp
@@ -1,7 +1,15 @@
 #include "gtest/gtest.h"
-//#include "main_func.cpp"
+#include "main_func.cpp"
 
 
-TEST(Password_from_list , no_threading_version){
-    ASSERT_EQ(1,1);
+TEST(Password_from_list , no_threading_version_file_with_passwords_exists){
+    std::string res1 = list_without_thread("Passwords\\passwords_list.txt" , 980537223); //exists 123456789a
+    std::string res2 = list_without_thread("Passwords\\passwords_list.txt" , 3116885959); //exists BUSTER
+    std::string res3 = list_without_thread("Passwords\\passwords_list.txt",	127154670); //exists bballer1
+    std::string res4 = list_without_thread("Passwords\\passwords_list.txt" , 429525991);// exists p00per
+
+    //ASSERT_EQ(res1  , "123456789a");
+   // ASSERT_EQ(res2 , "BUSTER");
+   // ASSERT_EQ(res3  , "bballer1");
+    //ASSERT_EQ(res4  , "p00per");
 }
